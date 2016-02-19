39857c87489272e628f79d457c90667a88b5cf2e
Sam Clayton <sam.clayton@gmail.com>
2016-02-18 21:50:23 -0600

    Fix JAVA_HOME for amd64, add ionic's expected version of Android SDK

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,7 @@ ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 #sudo add-apt-repository ppa:webupd8team/java
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y git openjdk-7-jdk ant expect
-sudo apt-get install -y nodejs
+sudo apt-get install -y git openjdk-7-jdk ant expect nodejs
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
@@ -17,10 +16,11 @@ tar -xzvf $ANDROID_SDK_FILENAME
 sudo chown -R vagrant android-sdk-linux/
 
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386" >> /home/vagrant/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /home/vagrant/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
 
 sudo npm install -g cordova ionic gulp bower
+# add gradle 2.2.1?
 
 sudo chown -R vagrant /usr/local/lib/node_modules
 # allow to uninstall/reinstall ionic w/o root
@@ -32,7 +32,7 @@ sudo apt-get autoremove
 
 expect -c '
 set timeout -1   ;
-spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-22,build-tools-22.0.1
+spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-22,android-23,build-tools-22.0.1
 expect { 
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
