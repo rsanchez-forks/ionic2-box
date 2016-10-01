a7d8c6af0a5991888dcb396f59f3ec0783141cb0
Maximiliano Villarreal <maximiliano.villarreal@gmail.com>
2016-10-01 14:44:37 -0300

    Update bootstrap.sh
    Various fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,11 @@ ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-# update from Ubuntu 10.04.3 to 10.04.4 or newer LTS releases
-sudo apt-get -y dist-upgrade
-sudo apt-get install -y git openjdk-7-jdk ant expect nodejs
+
+# update  Ubuntu to newer LTS releases
+#sudo apt-get -y dist-upgrade
+
+sudo apt-get install -y git openjdk-8-jdk ant expect nodejs
 # install 32-bit dependencies of Android build-tools
 sudo apt-get install -y lib32gcc1 libc6-i386 lib32z1 lib32stdc++6 lib32ncurses5 lib32gomp1 lib32z1-dev
 sudo ln -s /usr/bin/nodejs /usr/bin/node
@@ -17,16 +19,17 @@ tar -xzvf $ANDROID_SDK_FILENAME
 sudo chown -R vagrant android-sdk-linux/
 
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /home/vagrant/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /home/vagrant/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
 
 sudo npm install -g  node-gyp
-sudo npm install -g  node-sass
+sudo npm install -g  node-sass	
 sudo npm install -g  gulp 
 sudo npm install -g  cordova 
 sudo npm install -g  bower 
 sudo npm install -g  typings
 sudo npm install -g  ionic@2.0.0-beta.37 
+
 
 sudo chown -R vagrant /usr/lib/node_modules
 # allow to uninstall/reinstall ionic w/o root
